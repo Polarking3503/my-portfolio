8fe8c4d40b9f958b606459d21637d180b1ede080
Luis Jorge <92606623+Polarking3503@users.noreply.github.com>
2024-10-16 16:47:04 -0600

    Refactor email sending functionality to use environment variables for email credentials

---
+++ api/send-email.js
@@ -0,0 +1,38 @@
+import nodemailer from 'nodemailer';
+
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+        const { nombre, email, descripcion } = req.body;
+
+        const transporter = nodemailer.createTransport({
+            host: 'mail.apptlan.net',
+            port: 587,
+            secure: false,
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS,
+            },
+            tls: {
+                rejectUnauthorized: false,
+            },
+        });
+
+        const mailOptions = {
+            from: 'jorge@apptlan.net',
+            to: 'jorge@apptlan.net',
+            subject: 'Nuevo mensaje de contacto',
+            text: `Nombre: ${nombre}\nEmail: ${email}\nDescripción: ${descripcion}`,
+        };
+
+        try {
+            await transporter.sendMail(mailOptions);
+            res.status(200).json({ message: 'Correo enviado con éxito' });
+        } catch (error) {
+            console.error('Error al enviar el correo:', error);
+            res.status(500).json({ message: 'Error al enviar el correo' });
+        }
+    } else {
+        res.setHeader('Allow', ['POST']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+}
